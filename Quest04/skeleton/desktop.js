02f7cce4092766a4b029384ecd869950a3e82eed
나소영 <iamsoyoun9@naver.com>
2022-11-08 15:44:33 +0900

    221108

--- Quest04/skeleton/desktop.js
+++ Quest04/skeleton/desktop.js
@@ -4,6 +4,14 @@ class Desktop {
 
 class Icon {
 	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	constructor(name,type){
+		console.log('생성자')
+		this.name = name;
+		this.type = type;
+	}
+	create(name){
+	}
+
 };
 
 class Folder {
